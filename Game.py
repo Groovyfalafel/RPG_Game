f1601930058c8e08558476771b17ef66e70b72b8
Groovyfalafel <anhejjawi1@gmail.com>
2026-04-29 22:24:40 -0400

    Cleaned some stuff

--- Game.py
+++ Game.py
@@ -15,15 +15,6 @@ player_pos = pygame.Vector2(Width / 2, Height / 2)
 map_data = open("TestTiles/MapTest.txt", "r")
 map = []
 P1 = player(100,player_pos, 1100)
-water = pygame.image.load("TestTiles/water.png")
-grass = pygame.image.load("TestTiles/grass.png")
-path = pygame.image.load("TestTiles/path.png")
-grass_left = pygame.image.load("TestTiles/grass-path-left.png")
-grass_right = pygame.image.load("TestTiles/grass-path-right.png")
-grass_up = pygame.image.load("TestTiles/grass-path-up.png")
-grass_down = pygame.image.load("TestTiles/grass-path-down.png")
-grass_lbcorner = pygame.image.load("TestTiles/grass-corner-one.png")
-
 
 
 #load map data into the map
--- Settings.py
+++ Settings.py
@@ -1,6 +1,20 @@
+import pygame
+
 #settings.py
 
 Width = 1920
 Height = 1080
 Tile_Size = 32
 Fps = 60
+
+
+
+#Graphics
+water = pygame.image.load("TestTiles/water.png")
+grass = pygame.image.load("TestTiles/grass.png")
+path = pygame.image.load("TestTiles/path.png")
+grass_left = pygame.image.load("TestTiles/grass-path-left.png")
+grass_right = pygame.image.load("TestTiles/grass-path-right.png")
+grass_up = pygame.image.load("TestTiles/grass-path-up.png")
+grass_down = pygame.image.load("TestTiles/grass-path-down.png")
+grass_lbcorner = pygame.image.load("TestTiles/grass-corner-one.png")
